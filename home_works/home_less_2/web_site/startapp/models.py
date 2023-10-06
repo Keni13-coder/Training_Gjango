d980d03ba471a02900844e3362b739f73edafc1a
vlad <vlad.moldovanov.2014@mail.ru>
2023-10-07 02:17:00 +0300

    finish home admin

--- home_works/home_less_2/web_site/startapp/models.py
+++ home_works/home_less_2/web_site/startapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.db.models import Sum
+from django.utils.html import format_html
 # Create your models here.
 
 class User(models.Model):
@@ -10,6 +11,13 @@ class User(models.Model):
     phone_number = models.CharField(validators=[__phone_regex], max_length=17, blank=True)
     address = models.CharField(max_length=150)
     date_create_user = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = 'Пользователи'
+        verbose_name_plural = 'Пользователи'
+    
+    
     
     def __str__(self) -> str:
         return f'name: {self.name}, email: {self.email}, phone_number: {self.phone_number}, address: {self.address}'
@@ -23,6 +31,13 @@ class Product(models.Model):
     date_create_product = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(default='')
     # посмотреть как отображать картинку, возможно она по дефолту берёт из media
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = 'Продукты'
+        verbose_name_plural = 'Продукты'
+    
+    
     def __str__(self) -> str:
         return f'title: {self.title}, price: {self.price}, date: {self.date_create_product}'
 
@@ -31,12 +46,24 @@ class Product(models.Model):
         if self.image and hasattr(self.image, 'url'):
             return self.image.url
 
+    @property
+    def image_preview(self):
+        if self.image:
+            return format_html(f'<img src="{self.image.url}" style="max-width:200px; max-height:200px"/>')
+        return 'No image'
 
 class Order(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
     products = models.ManyToManyField('Product')
     total_cost = models.DecimalField(max_digits=6, decimal_places=2)
     date_create_order = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = 'Заказы'
+        verbose_name_plural = 'Заказы'
+    
+
     
     def __str__(self) -> str:
         return f'user_id: {self.user}, total_cost: {self.total_cost}'
